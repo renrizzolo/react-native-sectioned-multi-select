d33fcb1a9a00a7d80735badedfd63bed089bfe68
Ren <renrizmusic@gmail.com>
2020-10-01 10:05:47 +1000

    Fix icons (#206)
    * add icons prop name/size mapping
    
    * add IconRenderer
    
    * update types
    
    * remove react-native-vector-icons dep

--- exampleapp/__tests__/App.js
+++ exampleapp/__tests__/App.js
@@ -1,12 +1,24 @@
-import 'react-native';
-import React from 'react';
-import App from '../App';
-
+import 'react-native'
+import React from 'react'
+import SectionedMultiSelect from 'react-native-sectioned-multi-select'
+import App from '../App'
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer'
 
-it('renders correctly', () => {
-  const tree = renderer.create(
-    <App />
-  );
-});
+// 'it probably didn't break'
+
+const tree = renderer.create(<App />)
+it('renders example app correctly', () => {
+  tree
+})
+it('matches the snapshot', () => {
+  // make assertions on tree
+  expect(tree.toJSON()).toMatchSnapshot()
+})
+
+const testInstance = tree.root
+it('has an expected prop', () => {
+  expect(testInstance.findAllByType(SectionedMultiSelect)[0].props.single).toBe(
+    false
+  )
+})
